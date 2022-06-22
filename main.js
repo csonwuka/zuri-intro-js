869d963fc6ae5194079a87190bdf7a8c36c870ce
C.S. Onwuka <onwukasamm@gmail.com>
2022-06-22 12:23:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let fullname = "Samuel Onwuka.";
+let height = "5.8 ft.";
+let country = "Nigeria.";
+
+document.write('My name is ' + fullname);
+document.write(' I am ' + height);
+document.write(' I live in ' + country);
